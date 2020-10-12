9379b66bcccf0d0902286efa707395de8861f5d2
Daniel <daniel.fernex@dtellia.fr>
2020-10-12 20:15:27 +0200

    Plots almost done.

--- examples/helper.py
+++ examples/helper.py
@@ -8,7 +8,20 @@ import matplotlib.pyplot as plt
 import os
 from matplotlib import cm
 
+# Plotting parameters
+# ------------------------------------------------------------------------------
+params = {
+        'text.usetex': True,
+        'font.family': 'serif',
+         }
+
+plt.rcParams.update(params)
+
 FIGSIZE = (8,7)
+LW = 3          # line with
+LFONTSIZE = 18  # label font size
+TFONTSIZE = 15  # tick font size
+# ------------------------------------------------------------------------------
 
 def plot_phase_space(data,centroids,labels):
 
@@ -68,12 +81,242 @@ def plot_phase_space(data,centroids,labels):
     plt.show()
 
 
-def plot_time_series():
-    pass
-def plot_cpd():
-    pass
-def plot_autocorrelation():
-    pass
+def plot_time_series(t,x,t_hat,x_hat):
+
+    # Truncate at the same length
+    size = min(t.size,t_hat.size)
+    t = t[:size]
+    t_hat = t_hat[:size]
+    x = x[:size]
+    x_hat = x_hat[:size]
+
+    # Show only a specific time range
+    t_min,t_max = 45,60
+    idx_min, idx_max = np.argmin(abs(t-t_min)), np.argmin(abs(t-t_max))
+    t, t_hat, x, x_hat = t[idx_min:idx_max], t_hat[idx_min:idx_max], x[idx_min:idx_max], x_hat[idx_min:idx_max]
+    t -= t[0]
+    t_hat -= t_hat[0]
+
+    # Plot
+    label = ['x','y','z']
+
+    # Initialize figure
+    fig1, ax1 = plt.subplots(3, sharex=True, gridspec_kw={'hspace': 0},figsize=(6,4.5))
+    fig2, ax2 = plt.subplots(3, sharex=True, gridspec_kw={'hspace': 0},figsize=(6,4.5))
+
+    for i_dim in range(3):
+
+        ## --> Set plot lims
+        #Min = min(UCNM[:,i_dim].min(),UData[:,i_dim].min())
+        #Min = (Min + 0.3 * Min) if np.sign(Min) == -1 else (Min - Min * 0.3)
+        #print(Min)
+        #Max = max(UCNM[:,i_dim].max(),UData[:,i_dim].max())
+        #Max = Max + np.sign(Max) * 0.3 * Max
+        #yLim = [Min,Max]
+
+        # Start plot Data
+        # ----------------------------------------------------------------------
+        ax1[i_dim].plot(
+                t,
+                x[:,i_dim],
+                '-',
+                c='k',
+                lw=LW,
+                )
+
+        # Labels
+        if i_dim == 2:
+            ax1[i_dim].set_xlabel(r'$t$',fontsize=LFONTSIZE)
+        ax1[i_dim].set_ylabel(r'${}$'.format(label[i_dim]),fontsize=LFONTSIZE)
+
+        # Lims
+        ax1[i_dim].set_xlim([0,t[-1]])
+
+        # --> Ticks
+        if i_dim == 2:
+            ax1[i_dim].set_yticks([10,30])
+
+        # Start plot CNM
+        # ----------------------------------------------------------------------
+        ax2[i_dim].plot(
+                t_hat,
+                x_hat[:,i_dim],
+                '-',
+                c='r',
+                lw=LW,
+                )
+
+        # Labels
+        if i_dim == 2:
+            ax2[i_dim].set_xlabel(r'$t$',fontsize=LFONTSIZE)
+        ax2[i_dim].set_ylabel(r'${}$'.format(label[i_dim]),fontsize=LFONTSIZE)
+
+        # Lims
+        ax2[i_dim].set_xlim([0,t[-1]])
+
+        # Ticks
+        if i_dim == 2:
+            ax2[i_dim].set_yticks([10,30])
+
+    for i in range(3):
+        # Ticks
+        ax1[i].set_xticks([])
+        ax2[i].set_xticks([])
+        ax1[i].set_yticks([])
+        ax2[i].set_yticks([])
+
+        # remove labels and ticks from subplots that are not at the edge of the grid.
+        ax1[i].label_outer()
+        ax2[i].label_outer()
+
+    # Plot
+    fig1.tight_layout()
+    fig2.tight_layout()
+
+    #FigName1 = 'xyz-{}-S3.png'.format(PP.Label.replace('.0',''))
+    #FigName2 = 'xyz-{}-CNM-S3.png'.format(PP.Label.replace('.0',''))
+    #print('--> Saving {}'.format(FigName1))
+    #print('--> Saving {}'.format(FigName2))
+    #fig1.savefig(FigName1,dpi=500)
+    #fig2.savefig(FigName2,dpi=500)
+    #c1 = 'convert {F} -trim {F}'.format(F=FigName1)
+    #c2 = 'convert {F} -trim {F}'.format(F=FigName2)
+    #os.system(c1)
+    #os.system(c2)
+
+    plt.show()
+
+
+def plot_cpd(x,x_hat):
+    """Plot the cluster probability vector"""
+
+    # Re-cluster original and cnm data with 10 clusters only for clarity
+    from sklearn.cluster import KMeans
+    K = 10
+    kmeans = KMeans(n_clusters=K,max_iter=300,n_init=10,n_jobs=-1)
+    kmeans.fit(x)
+    labels = kmeans.labels_
+
+    # Predict cluster affiliation of the cnm data
+    labels_hat = kmeans.predict(x_hat)
+
+    # Probability distribution
+    q = np.bincount(labels).astype(float) / labels.size
+    q_hat = np.bincount(labels_hat).astype(float) / labels_hat.size
+
+    # --> Start plot
+    # ----------------------------------------------------------------------
+
+    fig = plt.figure(figsize=(6,4))
+
+    x = np.linspace(1,K,K)
+
+    # Plot
+    ax = plt.subplot(111)
+    ax.bar(
+            x-0.1,
+            q,
+            width=0.2,
+            align='center',
+            color='k',
+            )
+    ax.bar(
+            x+0.1,
+            q_hat,
+            width=0.2,
+            align='center',
+            color='r',
+            )
+
+    # Ticks
+    ax.set_xticks(np.arange(0,10)+1)
+    ax.set_yticks([])
+    ax.tick_params(labelsize=TFONTSIZE)
+
+
+    # Labels
+    #TickSize = 14
+    ax.set_xlabel(r'$c_k$',fontsize=LFONTSIZE)
+    ax.set_ylabel(r'$q$',fontsize=LFONTSIZE)
+
+    # Limits
+    max_q = max(q.max(),q_hat.max()) + max(q.max(),q_hat.max()) * 0.1
+    ax.set_ylim([0,max_q])
+
+    # Show
+    plt.tight_layout()
+    plt.show()
+
+def plot_autocorrelation(t,x,t_hat,x_hat):
+    """Plot the autocorrelation function"""
+
+    # Truncate at the same length
+    size = min(t.size,t_hat.size)
+    t = t[:size]
+    t_hat = t_hat[:size]
+    x = x[:size]
+    x_hat = x_hat[:size]
+
+    # Compute autocorrelation function
+    time_blocks = 40
+    compute_autocorrelation(t,x,time_blocks)
+
+def compute_autocorrelation(t,x,time_blocks):
+    """Compute the autocorrelation function"""
+
+    # Split into blocks of time time_blocks
+    n_blocks = int(t[-1]/time_blocks)
+    x_split = np.array_split(x,n_blocks)
+
+    # Truncate to give the same size
+    max_size = min([elt.shape[0] for elt in x_split])
+    x_split = [elt[:max_size] for elt in x_split]
+
+    # Loop over the blocks
+    for i_block,x_block in enumerate(x_split):
+
+        # fft
+        r = autocorrelation_fft(x_block)
+
+    r /= float(n_blocks)
+    plt.plot(r)
+    plt.show()
+
+def autocorrelation_fft(x):
+    """Compute the autocorrelation function using FFT and IFFT"""
+
+    n = x.shape[0]
+
+    # --> pad 0s to 2n-1
+    ext_size=2*n-1
+    # --> nearest power of 2
+    fsize=2**np.ceil(np.log2(ext_size)).astype('int')
+
+    # Loop over the dimensions
+    for i_dim in range(x.shape[1]):
+
+        # Remove the mean
+        x[:,i_dim] -= np.mean(x[:,i_dim])
+
+        # --> do fft and ifft
+        cf=np.fft.fft(x[:,i_dim],fsize)
+        sf=cf.conjugate()*cf
+        corr=np.fft.ifft(sf).real
+        corr=corr/n
+
+        if i_dim == 0:
+            r = corr[:int(x[:,i_dim].size)]
+        else:
+            r += corr[:int(x[:,i_dim].size)]
+    return r
+
+
+
+    #n = x.shape[0]
+
+    #for i_dim in range(U.shape[1]):
+
+
 
 def create_lorenz_data():
     """Create the Lorenz data"""
@@ -98,8 +341,6 @@ def create_lorenz_data():
     # Time settings
     T = 1000                     # total time
     n_points = T * 60            # number of samples
-    #T = 1000                     # total time
-    #n_points = T * 60            # number of samples
     t = np.linspace(0,T,n_points)# Time vector
     dt = t[1]-t[0]               # Time step
 
@@ -109,7 +350,44 @@ def create_lorenz_data():
 
     # remove the first 5% to keep only the 'converged' part which is in the ears
     points_to_remove = int(0.05 * n_points)
+
     return data[points_to_remove:,:], dt
 
+
+#
+#        # ----------------------------------------------------------------------
+#        # --> Start plot CNM
+#        # ----------------------------------------------------------------------
+#        plt.close()
+#        fig = plt.figure(figsize=(6,2.5))
+#        ax = fig.add_subplot(111)
+#
+#        ax.plot(
+#                x2,y2,
+#                '-',
+#                c='k',
+#                )
+#
+#        ## --> Labels
+#        FS = 30
+#        if i_dim == 2:
+#            ax.set_xlabel(r'$t$',fontsize=FS)
+#        ax.set_ylabel(r'${}$'.format(Label[i_dim]),fontsize=FS)
+#
+#        # --> Labels
+#        ax.set_xlim([0,tData[-1]-tData[0]])
+#
+#        # --> Invisible axes
+#        plt.xticks([])
+#        plt.yticks([])
+#
+#        # --> Plot
+#        plt.tight_layout()
+#        FigName = '{}-{}-CNM-SF'.format(Label[i_dim],PP.Label.replace('.0',''))
+#        print('--> Saving {}'.format(FigName))
+#        plt.savefig(FigName)
+#        plt.show()
+#
+#
 
 
--- examples/lorenz.py
+++ examples/lorenz.py
@@ -3,28 +3,52 @@
 import sys
 sys.path.insert(0,'../')
 from cnm import Clustering, TransitionProperties, Propagation
+import numpy as np
 
 def runLorenz():
 
     from helper import create_lorenz_data
     from sklearn.cluster import KMeans
 
-    # Create the Lorenz data
-    data, dt = create_lorenz_data()
-
     # CNM parameters:
     # ---------------
     K = 50 # Number of clusters
     L = 23 # Model order
 
-    # Clustering
-    # ----------
-    cluster_config = {
-            'data': data,
-            'cluster_algo': KMeans(n_clusters=K,max_iter=300,n_init=10,n_jobs=-1),
-            }
+    # Create the Lorenz data
+    import os
+    import pickle
+    if not os.path.exists('output/lorenz/cnm_data.npz'):
+        data, dt = create_lorenz_data()
+        t = np.arange(data.shape[0]) * dt
 
-    clustering = Clustering(**cluster_config)
+        # Clustering
+        # ----------
+        cluster_config = {
+                'data': data,
+                'cluster_algo': KMeans(n_clusters=K,max_iter=300,n_init=10,n_jobs=-1),
+                }
+
+        clustering = Clustering(**cluster_config)
+
+        with open('output/lorenz/clustering.pickle','wb') as f:
+            pickle.dump(clustering,f)
+
+
+        # SAVE
+        np.savez(
+                'output/lorenz/cnm_data',
+                data = data,
+                t = t,
+                dt = dt,
+                )
+    else:
+        with open('output/lorenz/clustering.pickle','rb') as f:
+            clustering = pickle.load(f)
+        D = np.load('output/lorenz/cnm_data.npz')
+        data = D['data']
+        t = D['t']
+        dt = D['dt']
 
     # Transition properties
     # ---------------------
@@ -44,48 +68,28 @@ def runLorenz():
             }
 
     ic = 0 # Index of the centroid to start in
-    t_total = 1000
-    dt = 0.001   # To spline-interpolate the centroid-to-centroid trajectory
+    t_total = 950
+    dt_hat = dt # To spline-interpolate the centroid-to-centroid trajectory
 
     propagation = Propagation(**propagation_config)
-    t_hat, x_hat = propagation.run(t_total,ic,dt)
+    t_hat, x_hat = propagation.run(t_total,ic,dt_hat)
 
     # Plot the results
     # ----------------
     from helper import (plot_phase_space, plot_time_series,plot_cpd,
                         plot_autocorrelation)
 
-    plot_phase_space(data,clustering.centroids,clustering.labels)
+    ## phase space
+    #plot_phase_space(data,clustering.centroids,clustering.labels)
 
+    ## time series
+    #plot_time_series(t,data,t_hat,x_hat)
 
-    #815         KM = KMeans(
-    #816                 n_clusters=self.nClusters,
-    #817                 max_iter=1000,
-    #818                 n_init=100,
-    #819                 n_jobs=-1,
-    #820                 )
-    #821
-    #822         # --> Fit the KMeans
-    #823         foo = ti.time()
-    #824         KM.fit(Ampl)
-    #825
-    #826         # --> Get the relevant attributes
-    #827         self.KMeans = KM
-    #828         return KM.labels_, KM.cluster_centers_
-    
-    
-    
-    ## Perform clustering
-    #cluster_options = {
-    #    Data foo,
-    #    opt2: bar,
-    #    algo=kmeans(K,...)
-    #    }
-    #
-    #cluster = Cluster(**cluster_options)
-    #process = MarkovProcess(**process_options,cluster)
-    #cnm = CNM(**...,process)
-    #cnm.run(tTotal,IC)
+    # cluster probability distribution
+    #plot_cpd(data,x_hat)
+
+    # autocorrelation function
+    plot_autocorrelation(t,data,t_hat,x_hat)
 
 if __name__== '__main__':
     runLorenz()
